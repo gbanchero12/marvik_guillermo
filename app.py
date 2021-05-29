80f0eb8e747788de9e5c7c6e6531322d72b801ab
Guillermo Banchero <gbanchero@technisys.com>
2021-05-29 15:59:53 -0300

    second commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import flask
+from flask import request, json
+from datetime import datetime
+import os
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = False
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():           
+    if request.method == 'POST':
+        
+        param = request.json['booleanParam']
+        
+        if param:        
+            return datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        else :
+            return datetime.today().strftime('%Y-%d-%m')
+    
+    if request.method == 'GET':
+        return '''<h1>Marvik</h1>
+        <p>Using the latest technology, processes and tools, we create custom solutions to find invisible insights and trends within your organization’s data. Differentiate from yourselves from the competition by scaling your business while reducing costs.</p>'''
+    
+        
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+app.run(threaded=True, port=33507)
---
+++ readme.txt
@@ -0,0 +1,10 @@
+# MARVIK
+
+# Documentation link:
+
+https://documenter.getpostman.com/view/3908658/TzXzDcXJ
+
+# Example Request
+
+curl --location --request POST 'localhost:5000' \
+--data-raw '{"booleanParam": false}'
